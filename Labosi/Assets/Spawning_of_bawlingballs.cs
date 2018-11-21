34f95af4605a974166ed470e3ece5c9306c331bc
SadLitleMan <43964778+SadLitleMan@users.noreply.github.com>
2018-11-21 12:16:35 +0100

    labos3

--- Labosi/Assets/Spawning_of_bawlingballs.cs
+++ Labosi/Assets/Spawning_of_bawlingballs.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Spawning_of_bawlingballs : MonoBehaviour
 {
+    public GameObject gameOverText;
+    public GameObject restartButton;
     public Text text;
     public float timeLeft = 30;
     public GameObject bowlingBall;
     public float maxWidth = 1;
     public float StartTime=2;
     public float time = 2;
+    public bool gamestart=false;
     // Use this for initialization
     void Start()
     {
@@ -45,21 +49,26 @@ public class Spawning_of_bawlingballs : MonoBehaviour
     void Update()
         
         {
-        if (timeLeft != 0)
-        {
-            if (time < 0)
-        {
-            Vector3 spawnPosition = new Vector3(Random.Range(-maxWidth, maxWidth), transform.position.y, 0.0f);
-            Quaternion spawnRotation = Quaternion.identity;
-            Instantiate(bowlingBall, spawnPosition, spawnRotation);
-            time = StartTime;
-        }
-        else
-            time = time - Time.deltaTime;
-            
-            }
-        
+            if (timeLeft != 0)
+            {
+                if (time < 0)
+                {
+                    Vector3 spawnPosition = new Vector3(Random.Range(-maxWidth, maxWidth), transform.position.y, 0.0f);
+                    Quaternion spawnRotation = Quaternion.identity;
+                    Instantiate(bowlingBall, spawnPosition, spawnRotation);
+                    time = StartTime;
+                }
+                else
+                    time = time - Time.deltaTime;
 
+            }
+            if (timeLeft <= 0)
+            {
+                gameOverText.SetActive(true);
+                restartButton.SetActive(true);
+            }
+
+        
     }
 
     void FixedUpdate()
@@ -71,6 +80,8 @@ public class Spawning_of_bawlingballs : MonoBehaviour
         text.text = "Time Left: \n" + Mathf.RoundToInt(timeLeft);
     }
 
+
+    
 }
 
 
